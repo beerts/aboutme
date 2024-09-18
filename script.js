4a6facc51a736262927aa7791115148a0a772f6a
Bernardo <133691749+beerts@users.noreply.github.com>
2024-09-18 11:34:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const terminalBody = document.querySelector('.terminal-body');
     const input = document.getElementById('command-input');
 
-    // Lista de comandos disponíveis
+ 
     const commands = [
         'ABOUTME            tell about my informations',
         'GITHUB             redirect for my github page',
@@ -11,21 +11,21 @@ document.addEventListener('DOMContentLoaded', function() {
         'ABOUTPAGE          how this page are made'
     ];
 
-    // Função para adicionar uma nova linha de comando com o campo de entrada
+
     function addCommandLine() {
         const newCommandLine = document.createElement('p');
         newCommandLine.className = 'prompt';
         newCommandLine.innerHTML = `C:\\Users\\Administrator> <input type="text" class="command-input" id="command-input" autofocus>`;
         terminalBody.appendChild(newCommandLine);
 
-        // Rola para o final da área de resposta
+
         terminalBody.scrollTop = terminalBody.scrollHeight;
     }
 
 
     input.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-            event.preventDefault(); // Evita o comportamento padrão do Enter
+            event.preventDefault();
             const command = input.value.trim();
 
             if (command) {
@@ -33,17 +33,17 @@ document.addEventListener('DOMContentLoaded', function() {
                     // Recarrega a página
                     location.reload();
                 } else if (command.toLowerCase() === 'help') {
-                    // Adiciona a lista de comandos disponíveis
+               
                     const helpMessage = commands.join('\n');
                     const responseLine = document.createElement('p');
                     responseLine.className = 'response';
                     responseLine.textContent = `AVAIABLE COMMANDS:\n\n${helpMessage}\n\n`;
                     terminalBody.appendChild(responseLine);
 
-                    // Adiciona uma nova linha de comando
+      
                     addCommandLine();
                 } else if (command.toLowerCase() === 'aboutme') {
-                    // Adiciona a resposta personalizada para ABOUTME
+   
                     const responseLine = document.createElement('p');
                     responseLine.className = 'response';
                     responseLine.textContent = `Hello, i'm Bernardo 23y Brazilian
@@ -55,41 +55,41 @@ Actually working at Information Technology (IT).\n
 TYPE 'HELP' FOR MORE COMMANDS\n`;
                     terminalBody.appendChild(responseLine);
                 } else if (command.toLowerCase() === 'aboutpage') {
-                    // Adiciona a resposta personalizada para ABOUTME
+ 
                     const responseLine = document.createElement('p');
                     responseLine.className = 'response';
                     responseLine.textContent = `THIS PAGE ARE MADE WITH HTML, CSS and JavaScript.\nCreated by Bernardo Teixeira - 2024.`;
                     terminalBody.appendChild(responseLine);
 
-                   // Adiciona uma nova linha de comando
+           
                    addCommandLine();
                 } else if (command.toLowerCase() === 'github') {
-                    // Redireciona para a página do GitHub em uma nova aba
-                    window.open('https://github.com/beerts', '_blank'); // Substitua com o URL do seu GitHub
+           
+                    window.open('https://github.com/beerts', '_blank'); 
                 } else if (command.toLowerCase() === 'linkedin') {
-                    // Redireciona para a página do LinkedIn em uma nova aba
-                    window.open('https://linkedin.com/in/bernardo-teixeira-341134272/', '_blank'); // Substitua com o URL do seu LinkedIn
+             
+                    window.open('https://linkedin.com/in/bernardo-teixeira-341134272/', '_blank'); 
                 } else {
-                    // Adiciona a linha de comando à área de resposta
+        
                     const promptLine = document.createElement('p');
                     promptLine.className = 'prompt';
                     promptLine.textContent = `C:\\Users\\Administrator> ${command}`;
                     terminalBody.appendChild(promptLine);
 
-                    // Adiciona uma linha de resposta simulada
+      
                     const responseLine = document.createElement('p');
                     responseLine.className = 'response';
                     responseLine.textContent = `\n'${command}' is not recognized as an internal command or external, an operable program or a batch file.\n\n`; // Substitua com lógica real
                     terminalBody.appendChild(responseLine);
 
-                    // Adiciona uma nova linha de comando
+           
                     addCommandLine();
                 }
 
-                // Limpa o campo de entrada
+
                 input.value = '';
 
-                // Reatribui o foco ao novo campo de entrada
+   
                 const newInput = document.querySelector('#command-input');
                 newInput.focus();
             }
